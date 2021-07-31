0fd0e3fbe8d9f91f12031f11e446e84c58c18d44
Jun Allard <jun.allard@uci.edu>
2021-07-31 10:42:30 -0700

    code to write small file in each directory so they sync nicely

--- makeFolders.sh
+++ makeFolders.sh
@@ -5,6 +5,7 @@ while read -r line;
 do
   folderName=$(printf "%02d" $i)"_"${line}
   echo $folderName;
-  mkdir $folderName;
+  #mkdir $folderName;
+  echo $folderName > $folderName/notes.txt
   i=$((i + 1));
 done < sessionlist.txt
